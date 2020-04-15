f405c43e95a52aac404e1e45cc63d69f64b31c3c
LMinh_global <1797747308@qq.com>
2020-04-15 13:13:58 +0800

    [SSH]Hello SSH

--- src/main/java/com/lmh/gitboot/controller/HelloController.java
+++ src/main/java/com/lmh/gitboot/controller/HelloController.java
@@ -10,4 +10,9 @@ public class HelloController {
     public String hello(){
         return "Hello Git";
     }
+
+    @RequestMapping("/ssh")
+    public String ssh(){
+        return "Hello SSH";
+    }
 }
